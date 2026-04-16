35f5977412394594217352f31ec0c8e84aae0939
SidickSino <sidickabdoulayesino1@gmail.com>
2026-04-16 15:06:57 +0200

    Restructure app by removing unused screens and components, and add new home and settings screens

---
+++ app/(tabs)/home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const home = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-2xl text-blue-500 ">home</Text>
+    </View>
+  );
+};
+
+export default home;
